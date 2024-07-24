052301deebbf6efba6dc234fd415911cf390fe0f
Brian Merritt <brian.merritt@jhuapl.edu>
2024-07-24 19:39:09 -0400

    adding update for missed taxids

--- bin/match_paths.py
+++ bin/match_paths.py
@@ -544,7 +544,6 @@ def main():
     # Aggregate data at the species level
     for top_level_key, entries in final_format.items():
         for val_key, data in entries.items():
-
             if top_level_key not in species_aggregated:
                 species_aggregated[top_level_key] = {
                     'key': top_level_key,
@@ -571,31 +570,35 @@ def main():
             #     # br
             # else:
             #     continue
-            gini_strain = getGiniCoeff(data['depths'], data['length'])
-            species_aggregated[top_level_key]['coeffs'].append(gini_strain)
-            species_aggregated[top_level_key]['taxids'].append(data['taxid'])
-            species_aggregated[top_level_key]['numreads'].append(data['numreads'])
-            species_aggregated[top_level_key]['coverages'].append(data['coverage'])
-            species_aggregated[top_level_key]['baseqs'].append(data['meanbaseq'])
-            species_aggregated[top_level_key]['accs'].append(data['accession'])
-            species_aggregated[top_level_key]['mapqs'].append(data['meanmapq'])
-            species_aggregated[top_level_key]['depths'].append(data['meandepth'])
-            if 'strain' in data:
-                species_aggregated[top_level_key]['strainslist'].append({
-                    "strainname":data['strain'],
-                    "fullname":data['name'],
-                    "subkey": val_key,
-                    "numreads": data['numreads'],
-                    "taxid": data['taxid'] if "taxid" in data else None,
-                })
-            else:
-                species_aggregated[top_level_key]['strainslist'].append({
-                    "strainname":val_key,
-                    "fullname":val_key,
-                    "subkey": val_key,
-                    "numreads": data['numreads'],
-                    "taxid": data['taxid'] if "taxid" in data else None,
-                })
+            try:
+                gini_strain = getGiniCoeff(data['depths'], data['length'])
+                species_aggregated[top_level_key]['coeffs'].append(gini_strain)
+                species_aggregated[top_level_key]['taxids'].append(data['taxid'])
+                species_aggregated[top_level_key]['numreads'].append(data['numreads'])
+                species_aggregated[top_level_key]['coverages'].append(data['coverage'])
+                species_aggregated[top_level_key]['baseqs'].append(data['meanbaseq'])
+                species_aggregated[top_level_key]['accs'].append(data['accession'])
+                species_aggregated[top_level_key]['mapqs'].append(data['meanmapq'])
+                species_aggregated[top_level_key]['depths'].append(data['meandepth'])
+                if 'strain' in data:
+                    species_aggregated[top_level_key]['strainslist'].append({
+                        "strainname":data['strain'],
+                        "fullname":data['name'],
+                        "subkey": val_key,
+                        "numreads": data['numreads'],
+                        "taxid": data['taxid'] if "taxid" in data else None,
+                    })
+                else:
+                    species_aggregated[top_level_key]['strainslist'].append({
+                        "strainname":val_key,
+                        "fullname":val_key,
+                        "subkey": val_key,
+                        "numreads": data['numreads'],
+                        "taxid": data['taxid'] if "taxid" in data else None,
+                    })
+            except Exception as e:
+                print(top_level_key, val_key,"___")
+                print(f"Error: {e}")
 
 
     # Calculate weighted means for aggregated data
