71e26981bda8804924f8673a38f05561561b3128
Brian Merritt <brian.merritt@jhuapl.edu>
2026-02-24 18:10:24 -0500

    adding default weights for gini fix

--- bin/match_paths.py
+++ bin/match_paths.py
@@ -340,21 +340,21 @@ def parse_args(argv=None):
         '--breadth_weight',
         metavar="BREADTHSCORE",
         type=float,
-        default=0.09,
+        default=0.15,
         help="value of weight for breadth of coverage in final TASS Score",
     )
     parser.add_argument(
         "--minhash_weight",
         metavar="MINHASHSCORE",
         type=float,
-        default=0.71,
+        default=0.38,
         help="value of weight for minhash signature reduction in final TASS Score",
     )
     parser.add_argument(
         "--gini_weight",
         metavar="GINIWEIGHT",
         type=float,
-        default=0.20,
+        default=0.47,
         help="value of weight for gini coefficient in final TASS Score",
     )
     parser.add_argument(
